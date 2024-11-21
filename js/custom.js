c94df7a734406ec5bffc64943ca96d8bb3cff417
joselik25 <137012581+joselik25@users.noreply.github.com>
2024-11-20 21:06:28 -0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'images/entrada.jpg' },
+              { src: 'images/entrada2.jpg' },
+              { src: 'images/entrada3.jpg' }
+          ],
+          timer: true, // Habilita el temporizador
+          delay: 7000, // 7 segundos entre imágenes
+          animation: 'fade', // Cambia el efecto
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
